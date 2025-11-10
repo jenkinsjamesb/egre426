3c42c576cb4c99fa58dad96378be100df17d7648
jenkinsjamesb <jenkins.j.brad@gmail.com>
2025-11-10 14:49:56 -0500

    Add insert_every function

--- common.py
+++ common.py
@@ -6,3 +6,7 @@ def int_to_bits(integer, length):
                 integer = (1 << length) + integer
 
         return format(integer, f'0{length}b')
+
+def insert_every(target, string, spacing):
+        '''Inserts string into target every spacing characters.'''
+        return string.join(target[i:i+spacing] for i in range(0,len(target),spacing))
